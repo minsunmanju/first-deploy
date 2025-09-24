0df1cf3a2bfe681983cbfdc8082da64fe89ac251
김민선 <pretty5320@naver.com>
2025-09-24 16:37:51 +0900

    이름정보 api에서 가져오기

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
 import Image from "next/image";
+// 서버 컴포넌트에서 직접 API 호출
+async function getResumeInfo() {
+  const res = await fetch(
+    "https://raw.githubusercontent.com/minsunmanju/first-deploy/refs/heads/main/service/resume_general_info_service.json"
+  );
+  // API 응답이 성공적인지 확인
+  if (!res.ok) {
+    // 응답이 실패하면 오류를 던져 Next.js가 오류 페이지를 보여주도록 함
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
 
-export default function Home() {
+export default async function Home() {
+  const data = await getResumeInfo();
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -16,7 +29,7 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             안녕하세요{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              김민선 입니다
+              {data.name} 입니다
             </code>
             .
           </li>
